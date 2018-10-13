ceb2df9f98998c05a9d9f223bd834de71b663d5c
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-10-13 20:50:47 -0300

    Add files via upload
    Envio do programa para controlar display LCD 16x2 ou 16x4 com controlador compatível com HD44780

---
+++ lcd1/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.d
@@ -0,0 +1,65 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Inc/stm32f1xx_hal_conf.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Inc/main.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cm3.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Inc/stm32f1xx_hal_conf.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Inc/main.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cm3.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+D:/Microcontroladores\ ARM/ST/Projetos/System\ Workbench/lcd1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
---
+++ lcd1/README.md
@@ -0,0 +1,6 @@
+Este simples programa faz a inicialização e escrita de um display 16x2 ou 16x4 que utiliza o controlador HD44780.
+
+Microcontrolador utilizado STM32F103C8
+
+Fonte:
+https://stm32withoutfear.blogspot.com/2016/10/stm32-lcd-wh16024-hd44780.html
