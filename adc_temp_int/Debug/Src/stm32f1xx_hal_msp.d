4c101aa3ff428dc59255e0c7cb1a5aba496ba52c
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-10-16 11:45:13 -0300

    Medição da temperatura interna do microcontrolador

---
+++ adc_temp_int/Debug/Src/stm32f1xx_hal_msp.d
@@ -0,0 +1,70 @@
+Src/stm32f1xx_hal_msp.o: ../Src/stm32f1xx_hal_msp.c \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Inc/stm32f1xx_hal_conf.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Inc/main.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cm3.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Inc/stm32f1xx_hal_conf.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Inc/main.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cm3.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/adc_temp_int/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
