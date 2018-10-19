1459128afd9c5b345eb525716321cc8557a43fd7
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-10-19 10:23:48 -0300

    Exemplo de utilização do encoder 2

---
+++ encoder2/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.d
@@ -0,0 +1,71 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Inc/stm32f1xx_hal_conf.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Inc/main.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cm3.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Inc/stm32f1xx_hal_conf.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Inc/main.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cm3.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/encoder2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
