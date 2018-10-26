61d389863e55ada9373e06e0ef4977e7864bf18f
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-10-26 08:50:08 -0300

    Add files via upload

---
+++ Blink_std/Debug/startup/subdir.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../startup/startup_stm32.s 
+
+OBJS += \
+./startup/startup_stm32.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/%.o: ../startup/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo $(PWD)
+	arm-none-eabi-as -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/Blink_std/StdPeriph_Driver/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/Blink_std/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/Blink_std/CMSIS/device" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/Blink_std/CMSIS/core" -g -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
