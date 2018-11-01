294508beb2e2b0231cc04ed31d23f861be8bba8d
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-11-01 17:26:29 -0300

    Add files via upload

---
+++ DS18B20_std1/Debug/src/delays.d
@@ -0,0 +1,4 @@
+src/delays.o: ../src/delays.c \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DS18B20_std1/inc/delays.h
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DS18B20_std1/inc/delays.h:
---
+++ DS18B20_std1/Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/delays.c \
+../src/ds18b20.c \
+../src/lcd.c \
+../src/main.c \
+../src/one_wire.c \
+../src/syscalls.c \
+../src/system_stm32f10x.c \
+../src/utils.c 
+
+OBJS += \
+./src/delays.o \
+./src/ds18b20.o \
+./src/lcd.o \
+./src/main.o \
+./src/one_wire.o \
+./src/syscalls.o \
+./src/system_stm32f10x.o \
+./src/utils.o 
+
+C_DEPS += \
+./src/delays.d \
+./src/ds18b20.d \
+./src/lcd.d \
+./src/main.d \
+./src/one_wire.d \
+./src/syscalls.d \
+./src/system_stm32f10x.d \
+./src/utils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DS18B20_std1/StdPeriph_Driver/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DS18B20_std1/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DS18B20_std1/CMSIS/device" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DS18B20_std1/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
