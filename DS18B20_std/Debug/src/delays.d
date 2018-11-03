fd505f5b4473d41bed0d788ea9716d1b18a0b414
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-11-02 21:03:01 -0300

    Add files via upload

---
+++ DS18B20_std/Debug/src/delays.d
@@ -0,0 +1,4 @@
+src/delays.o: ../src/delays.c \
+ D:/Microcontroladores/ARM/Projetos/System\ Workbench/STM32-System-Workbench-master/DS18B20_std/inc/delays.h
+
+D:/Microcontroladores/ARM/Projetos/System\ Workbench/STM32-System-Workbench-master/DS18B20_std/inc/delays.h:
---
+++ DS18B20_std/Debug/src/subdir.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/delays.c \
+../src/ds18b20.c \
+../src/lcd.c \
+../src/main.c \
+../src/one_wire.c \
+../src/syscalls.c \
+../src/system_stm32f10x.c 
+
+OBJS += \
+./src/delays.o \
+./src/ds18b20.o \
+./src/lcd.o \
+./src/main.o \
+./src/one_wire.o \
+./src/syscalls.o \
+./src/system_stm32f10x.o 
+
+C_DEPS += \
+./src/delays.d \
+./src/ds18b20.d \
+./src/lcd.d \
+./src/main.d \
+./src/one_wire.d \
+./src/syscalls.d \
+./src/system_stm32f10x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER -I"D:/Microcontroladores/ARM/Projetos/System Workbench/STM32-System-Workbench-master/DS18B20_std/StdPeriph_Driver/inc" -I"D:/Microcontroladores/ARM/Projetos/System Workbench/STM32-System-Workbench-master/DS18B20_std/inc" -I"D:/Microcontroladores/ARM/Projetos/System Workbench/STM32-System-Workbench-master/DS18B20_std/CMSIS/device" -I"D:/Microcontroladores/ARM/Projetos/System Workbench/STM32-System-Workbench-master/DS18B20_std/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
