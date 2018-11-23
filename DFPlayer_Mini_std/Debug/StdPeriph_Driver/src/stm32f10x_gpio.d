ace6485d7a1cf3d218c8120e07b409003d8300e2
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-11-23 17:21:32 -0200

    Add files via upload

---
+++ DFPlayer_Mini_std/Debug/StdPeriph_Driver/src/stm32f10x_gpio.d
@@ -0,0 +1,89 @@
+StdPeriph_Driver/src/stm32f10x_gpio.o: \
+ ../StdPeriph_Driver/src/stm32f10x_gpio.c \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/device/stm32f10x.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/core/core_cm3.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/device/system_stm32f10x.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_conf.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_adc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_bkp.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_can.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_cec.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_crc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dac.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dma.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_exti.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_flash.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_fsmc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_i2c.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_iwdg.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_pwr.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rcc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rtc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_sdio.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_spi.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_tim.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_usart.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_wwdg.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/misc.h \
+ D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rcc.h
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_gpio.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/device/stm32f10x.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/core/core_cm3.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/CMSIS/device/system_stm32f10x.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_conf.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_adc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_bkp.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_can.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_cec.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_crc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dac.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_dma.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_exti.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_flash.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_fsmc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_gpio.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_i2c.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_iwdg.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_pwr.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rcc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rtc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_sdio.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_spi.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_tim.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_usart.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_wwdg.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/misc.h:
+
+D:/Informações/Microcontroladores/ARM/Projetos/System\ workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc/stm32f10x_rcc.h:
---
+++ DFPlayer_Mini_std/Debug/src/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/DFPlayer.c \
+../src/delays.c \
+../src/main.c \
+../src/syscalls.c \
+../src/system_stm32f10x.c 
+
+OBJS += \
+./src/DFPlayer.o \
+./src/delays.o \
+./src/main.o \
+./src/syscalls.o \
+./src/system_stm32f10x.o 
+
+C_DEPS += \
+./src/DFPlayer.d \
+./src/delays.d \
+./src/main.d \
+./src/syscalls.d \
+./src/system_stm32f10x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DFPlayer_Mini_std/StdPeriph_Driver/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DFPlayer_Mini_std/inc" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DFPlayer_Mini_std/CMSIS/device" -I"D:/Informações/Microcontroladores/ARM/Projetos/System workbench/DFPlayer_Mini_std/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
