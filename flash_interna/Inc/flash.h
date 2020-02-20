e739bd4537c9a2e5aa063982802e39ce2c57a802
Fabrício Ribeiro <fabricio.engenheiro@gmail.com>
2020-02-20 09:32:09 -0300

    Exemplo para gravar na flash interna

--- flash_interna/Inc/flash.h
+++ flash_interna/Inc/flash.h
@@ -1,18 +1,19 @@
-/*
- * flash.h
- *
- *  Created on: Feb 19, 2020
- *      Author: fabricio
- */
+/*! @file   flash.h
+Biblioteca para gravar e ler a flash
+*/
 
 #ifndef FLASH_H_
 #define FLASH_H_
 
-// Include the appropriate HAL Library here
 #include "stm32f1xx_hal.h"
 #include <string.h>
 
-#define FLASH_STORAGE 0x08000000
+/*
+ * STM32F103C8
+ * FLASH INTERNA -> 0x08000000 - 0x0800FFFF
+ * Valor escolhido aleatoriamente: 0x08010000
+ */
+#define FLASH_STORAGE 0x08010000
 #define page_size 0x800
 
 void save_to_flash(uint8_t *data);
